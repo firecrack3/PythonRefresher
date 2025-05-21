ae8d819f84efddc2adf59f9bc4183d0f7fa582ae
firecrack3 <alfred.mavu@strathmore.edu>
2025-05-21 12:28:37 +0300

    commit 2

---
+++ Radius.py
@@ -0,0 +1,8 @@
+import math
+radius = float( input(("Enter radius: ")))
+Pi=math.pi
+volume= (4/3)* Pi * pow(radius,3)
+formatted_volume= round(volume,3)
+
+print(f"Volume of Sphere with {radius} cm is {formatted_volume}cm³")
+
