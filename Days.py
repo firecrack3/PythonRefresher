6afb81ca7ee40f63c28dd97bf9c7d629b27f7a19
firecrack3 <alfred.mavu@strathmore.edu>
2025-05-21 12:39:56 +0300

    Commit 3

--- Days.py
+++ Days.py
@@ -3,4 +3,4 @@ number_of_days= input(int("Enter the number of days: "))
 
 seconds_in_typed_day=number_of_days*seconds_per_day
 
-print(f"The number of seconds in "+number_of_days+ " days is "+ seconds_in_typed_day" seconds")
+print(f"The number of seconds in {number_of_days} days is { seconds_in_typed_day} seconds")
---
+++ Square.py
@@ -0,0 +1,7 @@
+import math
+side = float(input("Enter Square Side Length: "))
+
+Area = pow(side,2)
+Perimeter = side*4
+
+print(f"Area of square :{Area:.2f} cm2 \nPerimeter of Square: {Perimeter:.2f}cm")
