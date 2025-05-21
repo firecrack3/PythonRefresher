083fa223dbebad6fe2863d90b8610d5a98f93d66
firecrack3 <alfred.mavu@strathmore.edu>
2025-05-21 12:00:27 +0300

    Initial commit

---
+++ Days.py
@@ -0,0 +1,6 @@
+seconds_per_day=24*3600
+number_of_days= input(int("Enter the number of days: "))
+
+seconds_in_typed_day=number_of_days*seconds_per_day
+
+print(f"The number of seconds in "+number_of_days+ " days is "+ seconds_in_typed_day" seconds")
